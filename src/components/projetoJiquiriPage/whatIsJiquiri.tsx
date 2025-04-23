2afd33ee7d95af79062c945f14c93330dec525b9
Guh-paixao <gustavopaixao.dev@gmail.com>
2025-04-23 18:00:25 -0300

    feat: add EquipeMain and Techs components with GSAP animations; update WhatIsJiquiri for smoother transitions

--- src/components/projetoJiquiriPage/whatIsJiquiri.tsx
+++ src/components/projetoJiquiriPage/whatIsJiquiri.tsx
@@ -41,6 +41,7 @@ export default function WhatIsJiquiri() {
             whatIsJiquiriFooterRef.current,
             { opacity: 0, y: 80 },
             { opacity: 1, y: 0, duration: 1, ease: "power2.out" },
+            "-=0.5"
         );
 
     }, []);
