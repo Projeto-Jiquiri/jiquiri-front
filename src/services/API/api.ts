cfd5281ad37a2967568fba5bdfb6e4d7d9632b9a
Guh-paixao <gustavopaixao.dev@gmail.com>
2025-05-01 15:07:46 -0300

    fix: update baseURL to use API_URL environment variable

--- src/services/API/api.ts
+++ src/services/API/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL || undefined;
+const baseURL = process.env.API_URL || undefined;
 
 export const api = axios.create({
     baseURL: baseURL,
